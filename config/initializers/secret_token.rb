b6126dc74d2ee6d3ed0b43a7a1daaeefba2eda58
vibin.nair@gmail.com <vibin.nair@gmail.com>
2012-10-04 15:17:38 +0530

    Initial Project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmniauthTutorial::Application.config.secret_token = '5217a053fbe72304e9c557db3a4bf80330e7fa97396609c8783b029461491faee00efa6165c742858ea68340bb8a1979edec7f04fab410719ead5e1359befe36'
